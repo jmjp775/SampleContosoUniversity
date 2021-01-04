c0689d26b311fa8ba3e01552caea49e7627120c0
jmjp775 <jmjp775@gmail.com>
2021-01-04 11:44:59 -0500

    added properties to Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        //The addition of column FirstName changes the model backing the SchoolContext.
+        //The model backing the SchoolContext no longer matches the database.
+        //TODO: need to add a migration to fix this.
+
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:d}")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        public string FullName 
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;            
+            } 
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
