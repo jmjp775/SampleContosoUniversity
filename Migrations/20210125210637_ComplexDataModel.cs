8ff944a75e4749379315e3868c9d10d4c1d3aded
jmjp775 <jmjp775@gmail.com>
2021-01-30 10:11:54 -0500

    fixed to populate all db tables

--- Migrations/20210125210637_ComplexDataModel.cs
+++ Migrations/20210125210637_ComplexDataModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ContosoUniversity.Migrations
@@ -68,7 +69,7 @@ namespace ContosoUniversity.Migrations
                 columns: table => new
                 {
                     DepartmentID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
                     Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
                     Budget = table.Column<decimal>(type: "money", nullable: false),
                     StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
